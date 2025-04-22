bf00027ae0f027e7d2e8fb7fd9db2822b0b2db67
Matheus Oliveira <matheuso@alfaconsultorias.com.br>
2025-04-22 14:11:56 -0300

    Outra coisa importante para adicionar é fazer um pull de um repositório para uma pasta auxiliar

--- lib/features/analyzer/controller/analyzer_controller.dart
+++ lib/features/analyzer/controller/analyzer_controller.dart
@@ -1,6 +1,7 @@
 import 'package:mobx/mobx.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:process_run/process_run.dart';
+import 'dart:io';
 
 part 'analyzer_controller.g.dart';
 
@@ -21,15 +22,27 @@ abstract class _AnalyzerControllerBase with Store {
     final tempDir = await getTemporaryDirectory();
     final projectPath = '${tempDir.path}/$projectName';
 
-    await _runCommand('git', ['clone', repositoryLink, projectPath]);
+    if (await Directory(projectPath).exists()) {
+      logs.add('Projeto existente encontrado. Executando git pull...');
+      await _runGitPull(projectPath);
+    } else {
+      logs.add('Projeto não encontrado. Executando git clone...');
+      await _runCommand('git', ['clone', repositoryLink, projectPath]);
+    }
 
     if (!(await File('$projectPath/pubspec.yaml').exists())) {
+      logs.add('Criando novo projeto Flutter...');
       await _runCommand('flutter', ['create', projectPath]);
     }
 
     await _runFlutterCommands(projectPath);
 
     isAnalyzing = false;
+  }
+
+  Future<void> _runGitPull(String path) async {
+    await _runCommand('git', ['checkout', '.'], workingDir: path);
+    await _runCommand('git', ['pull'], workingDir: path);
   }
 
   Future<void> _runFlutterCommands(String path) async {
@@ -44,7 +57,9 @@ abstract class _AnalyzerControllerBase with Store {
 
   Future<void> _runCommand(String executable, List<String> arguments, {String? workingDir}) async {
     logs.add('Executando: $executable ${arguments.join(' ')}');
+
     final shell = Shell(workingDirectory: workingDir);
+
     await shell.runExecutableArguments(executable, arguments).then((result) {
       for (var line in result.outLines) {
         logs.add(line);
